9aa5c2dca78ab4c689b35da90ec4714f433b84ca
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-13 16:10:03 +0530

    Installed and set up tailwind acc to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className=" bg-orange-700 text-teal-500 text-3xl font-bold ">
+        Jai Shree Ram
+      </h1>
+      <h1 className=" bg-orange-900  text-3xl font-bold underline">
+        Jai Shree Ram
+      </h1>
+    </>
   );
 }
 
