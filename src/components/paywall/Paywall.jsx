a0659344eab67c6f3108bf32af505ba2d9667a0b
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-19 10:01:18 +0530

    Rendering different colored check box acc plan period.

--- src/components/paywall/Paywall.jsx
+++ src/components/paywall/Paywall.jsx
@@ -26,7 +26,7 @@ function Paywall() {
           )}
         </div>
         <div className="rightDiv  box-border h-fit w-8/12 flex flex-col gap-12 pr-12 font-white">
-          <div className="planContainer box-border h-full grid grid-cols-[3.5fr,1fr,1fr,1fr] gap-10 bg-black text-white text-2xl font-serif items-center justify-items-center	">
+          <div className="planContainer box-border h-full grid grid-cols-[3.5fr,1fr,1fr,1fr] gap-10 bg-black text-white text-2xl font-serif items-center justify-items-center">
             <div class="item"></div>
             <div class="item font-bold text-3xl">Mobile</div>
             <div class="item font-bold text-3xl">Super</div>
@@ -78,20 +78,18 @@ function Paywall() {
             <div class="item">2</div>
             <div class="item">4</div>
 
-            <div class="item justify-self-start justify-self-start">
-              Max video quality
+            <div class="item justify-self-start">Max video quality</div>
+            <div class="item">
+              <p className="text-center">HD</p>
+              <p className="text-center">720p</p>
             </div>
             <div class="item">
-              <p>HD</p>
-              <p>720p</p>
+              <p className="text-center">Full HD</p>
+              <p className="text-center">1080p</p>
             </div>
             <div class="item">
-              <p>Full HD</p>
-              <p>1080p</p>
-            </div>
-            <div class="item">
-              <p>4k</p>
-              <p>2160p</p>
+              <p className="text-center">4k</p>
+              <p className="text-center">2160p</p>
             </div>
 
             <div class="item justify-self-start">
@@ -100,11 +98,11 @@ function Paywall() {
                 Atmos available on select titles only
               </p>
             </div>
-            <div class="item">Stereo</div>
-            <div class="item">Dobly Atmos</div>
-            <div class="item">Dobly Atmos</div>
+            <div class="item ">Stereo</div>
+            <div class="item text-center">Dobly Atmos</div>
+            <div class="item text-center">Dobly Atmos</div>
           </div>
-
+          `
           <div className="planPeriodDiv flex w-6/12 h-16   justify-center gap-8 text-white bg-zinc-500 m-auto py-2 rounded-3xl">
             <div className="quaterly flex items-center justify-start gap-0 ">
               <div
@@ -119,7 +117,7 @@ function Paywall() {
                 Quaterly
               </div>
               {quaterlyPlanSelected && (
-                <CheckIcon className="w-8 text-white-900 text-2xl bg-purple-500 rounded-full" />
+                <CheckIcon className="w-8 text-white-900 text-2xl bg-yellow-500 rounded-full" />
               )}
             </div>
 
@@ -136,7 +134,7 @@ function Paywall() {
                 Yearly
               </p>
               {yearlyPlanSelected && (
-                <CheckIcon className="w-8 text-white-900 text-2xl bg-purple-500 rounded-full" />
+                <CheckIcon className="w-8 text-white-900 text-2xl bg-orange-500 rounded-full" />
               )}
             </div>
 
@@ -153,11 +151,10 @@ function Paywall() {
                 Monthly
               </p>
               {monthlyPlanSelected && (
-                <CheckIcon className="w-8 text-white-900 text-2xl bg-purple-500 rounded-full" />
+                <CheckIcon className="w-8 text-white-900 text-2xl bg-green-500 rounded-full" />
               )}
             </div>
           </div>
-
           <div className="costDiv flex h-fit text-white">
             <div className="mobileCost">
               <h3 className="heading">Mobile</h3>
@@ -172,7 +169,6 @@ function Paywall() {
               <p className="cost">cost</p>
             </div>
           </div>
-
           <button className="text-white">continue</button>
         </div>
       </div>
