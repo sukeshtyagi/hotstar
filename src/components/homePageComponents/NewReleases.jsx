77d05e1437bc213a38c07a71ef7281aa71d096d6
Sukesh Tyagi <sukeshtyagi05@gmail.com>
2024-03-25 07:03:30 +0530

    Code and images updated in TopRated and NewReleases for deployment on AWS

--- src/components/homePageComponents/NewReleases.jsx
+++ src/components/homePageComponents/NewReleases.jsx
@@ -6,15 +6,12 @@ function NewReleases() {
     <Cards
       category="New Releases"
       item1="/images/newReleases/nr1.jpg"
-      item2="/images/newReleases/nr4.jpg"
-      item3="/images/newReleases/nr6.jpeg"
+      item2="/images/newReleases/nr2.jpg"
+      item3="/images/newReleases/nr3.png"
       item4="/images/newReleases/nr4.jpg"
-      item5="/images/newReleases/nr9.jpg"
-      item6="/images/newReleases/nr6.jpeg"
-      item7="/images/newReleases/nr7.jpeg"
-      item8="/images/newReleases/nr1.jpg"
-      item9="/images/newReleases/nr9.jpg"
-      item10="/images/newReleases/nr10.jpg"
+      item5="/images/newReleases/nr5.jpg"
+      item6="/images/newReleases/nr6.jpg"
+      item7="/images/newReleases/nr7.jpg"
       link="/new-releases"
     />
   );
--- src/components/homePageComponents/TopRated.jsx
+++ src/components/homePageComponents/TopRated.jsx
@@ -5,13 +5,13 @@ function TopRated() {
   return (
     <Cards
       category="Top Rated"
-      item1="/images/topRated/tr1.jpeg"
+      item1="/images/topRated/tr1.jpg"
       item2="/images/topRated/tr2.jpg"
       item3="/images/topRated/tr3.jpg"
       item4="/images/topRated/tr6.jpg"
       item5="/images/topRated/tr5.jpg"
-      item6="/images/topRated/tr6.jpg"
-      item7="/images/topRated/tr1.jpeg"
+      item6="/images/topRated/tr2.jpg"
+      item7="/images/topRated/tr1.jpg"
       link="/new-releases"
     />
   );
